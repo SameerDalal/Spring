c5b342914ea2c327a85e60c1dfe9f68cd1285970
SameerDalal <sameerdalal747@gmail.com>
2022-02-06 16:29:57 -0500

    Initial commit

---
+++ src/main/java/com/example/demo/example/differentDataTypes.java
@@ -0,0 +1,199 @@
+package com.example.demo.example;
+
+public class differentDataTypes {
+
+    private String text = "This is a simplified example of what a user might see on the front end of a website:";
+    private String text2 = "These are some of the different Primitive Data Types in Java:";
+
+    private String byteInfo = "byte: Size: 8 bits, Minimum Whole Number Value: -128, Maximum Whole Number Value: 127";
+    private double aByte = 116;
+
+    private String shortInfo = "short: Size: 16 bits, Minimum Whole Number Value: -32,768, Maximum Whole Number Value: 32,767";
+    private short  aShort = 18382;
+
+    private String intInfo  = "int: Size: 4 bytes, Minimum Whole Number Value: -2,147,483,648, Maximum Whole Number Value: 2,147,483,647";
+    private int anInt = 1192884;
+
+    private String longInfo = "long: Size: 8 bytes, Minimum Whole Number Value: -9,223,372,036,854,775,808, Maximum Whole Number Value: 9,223,372,036,854,775,807";
+    private long aLong = 8437029;
+
+    private String floatInfo =  "float: Size: 4 bytes, Stores numbers with 6 to 7 decimal digits";
+    private float aFloat = (float)3.141592;
+
+    private String doubleInfo = "double: Size: 8 bytes, Stores numbers with 15 decimal digits";
+    private double aDouble = 2.7182818284590;
+
+    private String booleanInfo = "boolean: Size: 1 bit, Stores true or false values";
+    private boolean aBoolean = true;
+
+    private String charInfo = "Stores a character value";
+    private char aChar = '@';
+
+    public differentDataTypes(){
+    }
+
+    public differentDataTypes(java.lang.String text, java.lang.String text2, java.lang.String byteInfo, double aByte, java.lang.String shortInfo, short aShort, java.lang.String intInfo, int anInt, java.lang.String longInfo, long aLong, java.lang.String floatInfo, float aFloat, java.lang.String doubleInfo, double aDouble, java.lang.String booleanInfo, boolean aBoolean, java.lang.String charInfo, char aChar) {
+        this.text = text;
+        this.text2 = text2;
+        this.byteInfo = byteInfo;
+        this.aByte = aByte;
+        this.shortInfo = shortInfo;
+        this.aShort = aShort;
+        this.intInfo = intInfo;
+        this.anInt = anInt;
+        this.longInfo = longInfo;
+        this.aLong = aLong;
+        this.floatInfo = floatInfo;
+        this.aFloat = aFloat;
+        this.doubleInfo = doubleInfo;
+        this.aDouble = aDouble;
+        this.booleanInfo = booleanInfo;
+        this.aBoolean = aBoolean;
+        this.charInfo = charInfo;
+        this.aChar = aChar;
+    }
+
+    public java.lang.String getText() {
+        return text;
+    }
+
+    public void setText(java.lang.String text) {
+        this.text = text;
+    }
+
+    public java.lang.String getText2() {
+        return text2;
+    }
+
+    public void setText2(java.lang.String text2) {
+        this.text2 = text2;
+    }
+
+    public java.lang.String getByteInfo() {
+        return byteInfo;
+    }
+
+    public void setByteInfo(java.lang.String byteInfo) {
+        this.byteInfo = byteInfo;
+    }
+
+    public double getaByte() {
+        return aByte;
+    }
+
+    public void setaByte(double aByte) {
+        this.aByte = aByte;
+    }
+
+    public java.lang.String getShortInfo() {
+        return shortInfo;
+    }
+
+    public void setShortInfo(java.lang.String shortInfo) {
+        this.shortInfo = shortInfo;
+    }
+
+    public short getaShort() {
+        return aShort;
+    }
+
+    public void setaShort(short aShort) {
+        this.aShort = aShort;
+    }
+
+    public java.lang.String getIntInfo() {
+        return intInfo;
+    }
+
+    public void setIntInfo(java.lang.String intInfo) {
+        this.intInfo = intInfo;
+    }
+
+    public int getAnInt() {
+        return anInt;
+    }
+
+    public void setAnInt(int anInt) {
+        this.anInt = anInt;
+    }
+
+    public java.lang.String getLongInfo() {
+        return longInfo;
+    }
+
+    public void setLongInfo(java.lang.String longInfo) {
+        this.longInfo = longInfo;
+    }
+
+    public long getaLong() {
+        return aLong;
+    }
+
+    public void setaLong(long aLong) {
+        this.aLong = aLong;
+    }
+
+    public java.lang.String getFloatInfo() {
+        return floatInfo;
+    }
+
+    public void setFloatInfo(java.lang.String floatInfo) {
+        this.floatInfo = floatInfo;
+    }
+
+    public float getaFloat() {
+        return aFloat;
+    }
+
+    public void setaFloat(float aFloat) {
+        this.aFloat = aFloat;
+    }
+
+    public java.lang.String getDoubleInfo() {
+        return doubleInfo;
+    }
+
+    public void setDoubleInfo(java.lang.String doubleInfo) {
+        this.doubleInfo = doubleInfo;
+    }
+
+    public double getaDouble() {
+        return aDouble;
+    }
+
+    public void setaDouble(double aDouble) {
+        this.aDouble = aDouble;
+    }
+
+    public java.lang.String getBooleanInfo() {
+        return booleanInfo;
+    }
+
+    public void setBooleanInfo(java.lang.String booleanInfo) {
+        this.booleanInfo = booleanInfo;
+    }
+
+    public boolean isaBoolean() {
+        return aBoolean;
+    }
+
+    public void setaBoolean(boolean aBoolean) {
+        this.aBoolean = aBoolean;
+    }
+
+    public java.lang.String getCharInfo() {
+        return charInfo;
+    }
+
+    public void setCharInfo(java.lang.String charInfo) {
+        this.charInfo = charInfo;
+    }
+
+    public char getaChar() {
+        return aChar;
+    }
+
+    public void setaChar(char aChar) {
+        this.aChar = aChar;
+    }
+}
